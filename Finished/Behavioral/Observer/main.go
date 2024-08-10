fee73fccfd49a03012499fedfa3e76e8b56dd283
Alok Tripathi <gnsalok@gmail.com>
2024-08-10 14:57:11 +0530

    feat: adding obeverser pattern

--- Finished/Behavioral/Observer/main.go
+++ Finished/Behavioral/Observer/main.go
@@ -1,5 +1,19 @@
 package main
 
+/*
+
+// Link : https://refactoring.guru/design-patterns/observer/go/example#example-0
+
+- Subject, the instance which publishes an event when anything happens.
+- Observer, which subscribes to the subject events and gets notified when they happen.
+
+* Subject ; Concrete Subject -> Item
+* Observer ; Concrete Observer -> Customer
+* main.go -> client
+
+
+*/
+
 func main() {
 
 	shirtItem := newItem("Nike Shirt")
--- Finished/Behavioral/Observer/subject.go
+++ Finished/Behavioral/Observer/subject.go
@@ -1,7 +1,5 @@
 package main
 
-// Link : https://refactoring.guru/design-patterns/observer/go/example#example-0
-
 // Define the interface for the observable type
 type Observable interface {
 	register(obs Observer)
