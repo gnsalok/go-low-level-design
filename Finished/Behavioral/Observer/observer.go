da476fd5c27f4cfc6cc856a180fd4a2c8cb699a7
Alok Tripathi <aloktripathi@Aloks-MacBook-Pro.local>
2024-08-10 14:40:03 +0530

    feat: adding obeverser pattern

--- Finished/Behavioral/Observer/observer.go
+++ Finished/Behavioral/Observer/observer.go
@@ -1,18 +1,6 @@
 package main
 
-import "fmt"
-
-// Define the interface for an observer type
-type observer interface {
-	onUpdate(data string)
-}
-
-// Our DataListener observer will have a name
-type DataListener struct {
-	Name string
-}
-
-// To conform to the interface, it must have an onUpdate function
-func (dl *DataListener) onUpdate(data string) {
-	fmt.Println("Listener:", dl.Name, "got data change:", data)
+type Observer interface {
+	update(string)
+	getID() string
 }
