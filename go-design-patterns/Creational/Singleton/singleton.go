64b89860cab39560120ea29951d49099470a1e38
Alok Tripathi <gnsalok@gmail.com>
2026-01-04 22:06:23 +0530

    refactor: singleton pattern

--- go-design-patterns/Creational/Singleton/singleton.go
+++ go-design-patterns/Creational/Singleton/singleton.go
@@ -1,38 +1,29 @@
 package main
 
-import (
-	"fmt"
-	"sync"
+import "sync"
+
+// NOTE: Singletons are rarely necessary in Go. Prefer passing dependencies
+// explicitly. When needed, sync.Once provides safe lazy initialization.
+
+type DatabaseConnection struct{}
+
+func (d *DatabaseConnection) Query(sql string) {
+	// Database operations
+	_ = sql
+}
+
+var (
+	dbInstance *DatabaseConnection
+	once       sync.Once
 )
 
-// MyLogger is the struct we want to make a singleton
-type MyLogger struct {
-	loglevel int
-}
-
-// Log a message using the logger
-func (l *MyLogger) Log(s string) {
-	fmt.Println(l.loglevel, ":", s)
-}
-
-// SetLogLevel sets the log level of the logger
-func (l *MyLogger) SetLogLevel(level int) {
-	l.loglevel = level
-}
-
-// the logger instance
-var logger *MyLogger
-
-// use the sync package to enforce goroutine safety
-var once sync.Once
-
-// the getLoggerInstance function provides global access to the
-// logger class instance
-func getLoggerInstance() *MyLogger {
+func GetDatabaseConnection() *DatabaseConnection {
 	once.Do(func() {
-		fmt.Println("Creating Logger Instance")
-		logger = &MyLogger{}
+		dbInstance = &DatabaseConnection{}
 	})
-	fmt.Println("Returning Logger Instance")
-	return logger
+	return dbInstance
 }
+
+// Usage:
+// db := GetDatabaseConnection()
+// db.Query("SELECT * FROM users")
