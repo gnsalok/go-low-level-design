1109c8d5a8fbd935dc3327a1c3c3b75d36831d37
Alok Tripathi <gnsalok@gmail.com>
2026-01-04 23:23:03 +0530

    refactor: builder pattern

--- go-design-patterns/Creational/Builder/example.go
+++ go-design-patterns/Creational/Builder/example.go
@@ -4,7 +4,8 @@ import "fmt"
 
 /*
 Builder Pattern
-- The Builder pattern is a creational design pattern that allows you to create complex objects step by step.
+- TA builder is a helper that lets you create a complex object step by step without worrying about the order or messy construction details.
+- It's used when an object has many optional parts or configuration choices.
 - It separates the construction of a complex object from its representation, allowing the same construction process to create different representations.
 - The Builder pattern is particularly useful when an object needs to be created with many optional parameters or when the construction process involves multiple steps.
 - In this example, we will create a NotificationBuilder that allows us to build a Notification object step by step.
